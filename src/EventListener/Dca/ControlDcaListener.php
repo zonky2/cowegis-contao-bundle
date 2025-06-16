74c72547218beffdcc26d25dfb1947df3532d348
David Molineus <mail@netzmacht.de>
2025-06-16 15:44:45 +0200

    Ensure type safety for Contao 4.13

--- src/EventListener/Dca/ControlDcaListener.php
+++ src/EventListener/Dca/ControlDcaListener.php
@@ -58,7 +58,7 @@ final class ControlDcaListener extends AbstractListener
     public function layerOptions(MultiColumnWizard $multiColumnWizard): array
     {
         /** @psalm-suppress DocblockTypeContradiction */
-        if (0 === $multiColumnWizard->dataContainer->currentPid) {
+        if (0 === ((int) $multiColumnWizard->dataContainer->currentPid)) {
             return [];
         }
 
